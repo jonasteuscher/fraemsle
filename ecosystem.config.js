b74db9284c51584a74ca2d293e155984f22524a1
Teuscher Jonas, B2B-BAN-ABP-CEN-CA1 <Jonas.Teuscher@swisscom.com>
2023-10-21 11:14:11 +0200

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       user : 'ubuntu',
       host : '35.158.86.16',
       ref  : 'origin/master',
-      repo : 'https://github.com/jonasteuscher/fraemsle.git', 
+      repo : 'git@github.com:jonasteuscher/fraemsle.git', 
       path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
