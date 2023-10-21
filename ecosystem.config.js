f552867fa671ae37de95cc3b392c982943389f07
Teuscher Jonas, B2B-BAN-ABP-CEN-CA1 <Jonas.Teuscher@swisscom.com>
2023-10-21 11:00:18 +0200

    Add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }, ],
+
+  deploy : {
+    production : {
+      key: 'ec2_keypair.pem',
+      user : 'ubuntu',
+      host : '35.158.86.16',
+      ref  : 'origin/master',
+      repo : 'https://github.com/jonasteuscher/fraemsle.git', 
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
